Fixed rolling variance

main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def generate_time_series(length):
-    return np.random.normal(1, 2, length)
+    return np.random.normal(10, 5, length)
 
 
 def compute_mean(time_series, window_length):
@@ -41,6 +41,7 @@ def compute_variance(time_series, window_length):
 
 def compute_rolling_variance(time_series, window_length):
     m = np.mean(time_series[0:window_length])
+    # s = np.sum(time_series[0:window_length])
     means = []
     means.append(m)
     v = np.var(time_series[0:window_length])
@@ -49,28 +50,34 @@ def compute_rolling_variance(time_series, window_length):
     for i in range(0, len(time_series)):
         if i >= window_length:
             m_new = m + (1 / (window_length)) * (time_series[i] - time_series[i - window_length])
-            means.append(m_new)
+            v_new = v + m**2 + (1/window_length)* (time_series[i]**2 - time_series[i - window_length]**2) - m_new**2
             m = m_new
-            v_new = v + (1 / (window_length)) * (
-            (time_series[i] - m_new) ** 2 - (time_series[i - window_length] - m) ** 2)
-            variances.append(v_new)
             v = v_new
+            variances.append(v_new)
 
     return variances
 
 
 def main():
     length = 100
-    window_length = 30
+    window_length = 10
     ts = generate_time_series(length)
     means = compute_mean(ts, window_length)
     variances = compute_variance(ts, window_length)
     rolling_means = compute_rolling_mean(ts, window_length)
     rolling_variances = compute_rolling_variance(ts, window_length)
+
+    print(means)
+    print(len(means))
+    print(rolling_means)
+    print(len(rolling_means))
+
+
     print(variances)
     print(len(variances))
     print(rolling_variances)
     print(len(rolling_variances))
+
 
 
 if __name__ == '__main__':